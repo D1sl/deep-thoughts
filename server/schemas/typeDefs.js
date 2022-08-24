a4375e705b95c7a9e241a14f335d19f53cdb66b2
D1sl <94911312+D1sl@users.noreply.github.com>
2022-08-24 17:21:04 +0100

    Queries for thoughts and users as well as individuals for each

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,7 +2,45 @@
 const { gql } = require('apollo-server-express');
 
 // create our typeDefs
-const typeDefs = gql``;
+const typeDefs = gql`
+
+    type Thought {
+        _id: ID
+        thoughtText: String
+        createdAt: String
+        username: String
+        reactionCount: Int
+        reactions: [Reaction]
+    }
+
+    type Reaction {
+        _id: ID
+        reactionBody: String
+        createdAt: String
+        username: String
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        friendCount: Int
+        thoughts: [Thought]
+        friends: [User]
+    }
+
+    type Query {
+        users: [User]
+        user(username: String!): User
+        thoughts(username: String): [Thought]
+        thought(_id: ID!): Thought
+    }
+
+`;
+
+// thoughts: [Thought] would return everything but when we define username: String in parentheses for the thoughts query, we are defining that this query COULD receive a parameter IF we wanted it to. This has to also be defined in the resolvers.
+
+// "type Query" lists the types of queries that can be run and what they return
 
 // export the typeDefs
 module.exports = typeDefs;
