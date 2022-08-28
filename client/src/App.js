6b0ad1bf55a8b5ebc86066c821c68a56df8fe0f1
D1sl <94911312+D1sl@users.noreply.github.com>
2022-08-28 14:12:17 +0100

    Adds authentication and profile page for logged in user

--- client/src/App.js
+++ client/src/App.js
@@ -14,16 +14,26 @@ import Signup from './pages/Signup';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-
+// Apollo and Authentication
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-
+import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
 
