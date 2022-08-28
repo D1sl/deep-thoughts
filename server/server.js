3705ea845c0b7c37c26ba1ed938441e32c580ebd
D1sl <94911312+D1sl@users.noreply.github.com>
2022-08-28 17:06:25 +0100

    preparing for Heroku deploy

--- server/server.js
+++ server/server.js
@@ -50,19 +50,3 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
 // Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
-
-
-// const express = require('express');
-// const db = require('./config/connection');
-
-// const PORT = process.env.PORT || 3001;
-// const app = express();
-
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
-
-// db.once('open', () => {
-//   app.listen(PORT, () => {
-//     console.log(`API server running on port ${PORT}!`);
-//   });
-// });
